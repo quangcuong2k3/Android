72cfac4a1e7f5ab031def20e2a15c8ea3ef19afc
Cuong <cuongquangle539@gmail.com>
2025-04-10 08:46:41 +0700

    4

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View >
+        <Text style={styles.header}>Xin chao</Text>
+        <Text style={styles.parent}>Hello 2
+          <Text style={styles.child}>BLalala</Text>
+        </Text>
+      </View>
+      <Text>hello!</Text>
+      <Text style={styles.hello1}>hell2o!</Text>
+
     </View>
   );
 }
@@ -17,4 +24,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: "red", fontSize: 60,
+    borderColor: "green",
+    borderWidth: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  parent: {
+    fontSize: 60,
+    color: "green"
+  },
+  child: {
+    fontSize: 30,
+    color: "pink"
+  }
 });
