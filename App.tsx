ce91042b0f2b8b9248b15b37199c8b92a2e6fe83
Cuong <cuongquangle539@gmail.com>
2025-04-10 20:25:44 +0700

    7

--- App.tsx
+++ App.tsx
@@ -1,30 +1,50 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 export default function App() {
 
-  const [count, setCount] = useState<number>(0);
-
+  const [students, setStudents] = useState(
+    [
+      { id: 1, name: "Cuong", age: 18 },
+      { id: 2, name: "Cuong2", age: 11 },
+      { id: 3, name: "Cuong3", age: 12 },
+      { id: 4, name: "Cuong4", age: 13 },
+      { id: 5, name: "Cuong5", age: 14 },
+      { id: 6, name: "Cuong6", age: 15 },
+      { id: 7, name: "Cuong7", age: 16 },
+      { id: 8, name: "Cuong8", age: 17 },
+    ]
+  )
   //jsx
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 60, fontWeight: "600" }}>
-        count ={count}
-      </Text>
-      <View>
-        <Button title='Increase' onPress={() => setCount(count + 1)} />
-      </View>
-    </View>
+      <Text style={{ fontSize: 60 }}>Hello world</Text>
+      <ScrollView>
+        {students.map((item, index) => {
+          return (
+            <View key={item.id} style={{
+              padding: 15,
+              backgroundColor: "pink",
+              marginBottom: 10
+            }}>
+              <Text>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingHorizontal: 20,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 
 });
