660be94cb9049ba98abd0259bc5f5861413440f3
Cuong <cuongquangle539@gmail.com>
2025-04-11 15:37:23 +0700

    10

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Button, FlatList, ScrollView,
+  StyleSheet, Text, TextInput, View, TouchableOpacity,
+  Pressable
+} from 'react-native';
 
 interface ITodo {
   id: number,
@@ -14,11 +18,18 @@ export default function App() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   const handleAddTodo = () => {
-    if (!todo) return;
+    if (!todo) {
+      alert("empty todo")
+      return;
+    };
     setListTodo([...listTodo,
     { id: randomInteger(2, 20000000), name: todo, }
     ]);
     setTodo("")
+  }
+  const deleteTodo = (id: number) => {
+    const newTodos = listTodo.filter(item => item.id !== id);
+    setListTodo(newTodos)
   }
   //jsx
   return (
@@ -45,11 +56,18 @@ export default function App() {
           keyExtractor={item => item.id + ""}
           renderItem={({ item }) => {
             return (
-              <Text style={styles.todoItem}>{item.name}</Text>
+              <Pressable
+                onPress={() => deleteTodo(item.id)}
+                style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
+              >
+                <Text
+                  style={styles.todoItem}>{item.name}
+                </Text>
+              </Pressable>
+
             );
           }}
         />
-        <Text> {JSON.stringify(listTodo)}</Text>
       </View>
     </View >
   );
